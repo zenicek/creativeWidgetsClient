e6870d5fce4d2a7ceb5ce7784736cf5ef5d96d81
Eugen Nikolajev <zenicek@gmail.com>
2021-11-11 11:33:47 +0100

    complete icons list

--- client/src/Components/WidgetBuilder/ElementsList/Elements.list.js
+++ client/src/Components/WidgetBuilder/ElementsList/Elements.list.js
@@ -2,11 +2,21 @@ import './Elements.list.css';
 import icons from './Icons/Icons';
 
 export function ElementsList() {
+  const list = icons.map((icon, index) => {
+    return (
+      <li key={index} className="icon-ctn">
+        <div>
+          <img src={icon.icon} alt={icon.text} className="icon-img" />
+          <span className="option-text">{icon.text}</span>
+        </div>
+      </li>
+    );
+  });
+
   return (
     <ul>
-      <li>
-        <img src={icons[0].icon} />{' '}
-      </li>
+      <h4>Select element and drag</h4>
+      {list}
     </ul>
   );
 }
--- client/src/Components/WidgetBuilder/ElementsList/Icons/Icons.js
+++ client/src/Components/WidgetBuilder/ElementsList/Icons/Icons.js
@@ -1,9 +1,45 @@
 import list from './SvgIcons/list-icon.svg';
+import slider from './SvgIcons/slider-icon.svg';
+import value from './SvgIcons/value-icon.svg';
+import text from './SvgIcons/text-icon.svg';
+import checkbox from './SvgIcons/checkbox-icon.svg';
+import radio from './SvgIcons/radio-icon.svg';
+import button from './SvgIcons/button-icon.svg';
+import image from './SvgIcons/image-icon.svg';
+//import calculation from './SvgIcons/calculation-icon.svg';
 
 const icons = [
   {
+    icon: value,
+    text: 'Value field',
+  },
+  {
     icon: list,
     text: 'List',
+  },
+  {
+    icon: text,
+    text: 'Text',
+  },
+  {
+    icon: slider,
+    text: 'Slider',
+  },
+  {
+    icon: checkbox,
+    text: 'Checkbox',
+  },
+  {
+    icon: radio,
+    text: 'Radio selector',
+  },
+  {
+    icon: button,
+    text: 'Button',
+  },
+  {
+    icon: image,
+    text: 'Image',
   },
 ];
 
