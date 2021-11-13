a5608f59ce302531ac36d4df7c41fb1417b704f3
Eugen Nikolajev <zenicek@gmail.com>
2021-11-13 17:25:02 +0100

    list created

--- client/src/Components/InputElements/List/List.js
+++ client/src/Components/InputElements/List/List.js
@@ -1,15 +1,28 @@
 import { useContext } from 'react';
+import { IndividualWidget } from '../../../Utils/Contexts';
 import './List.css';
 
 export function List({ id }) {
+  const { updateElement, findElement } = useContext(IndividualWidget);
+
+  const element = { ...findElement(id) };
+
+  const options = () => {
+    if (element.list.length > 0) {
+      return element.list.map(item => {
+        return <option value={item.onValue}>item.optionName</option>;
+      });
+    } else {
+      return <option value="no-data">Currently No Options</option>;
+    }
+  };
   return (
     <div className="input-ctn">
       <label htmlFor="widget-list">List</label>
       <div>
-        <input list="datalist" className="input" id="widget-list"></input>
-        <datalist id="datalist">
-          <option value="TODO CHANGE THIS TO REAL ARRAY"></option>
-        </datalist>
+        <select className="input" id="widget-list">
+          {options()}
+        </select>
       </div>
     </div>
   );
