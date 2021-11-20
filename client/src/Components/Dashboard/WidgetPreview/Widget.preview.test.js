9281983351d79792c1df1493645463046ed95b90
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2021-11-20 15:25:34 +0100

    test: add tests in WidgetPreview

--- client/src/Components/Dashboard/WidgetPreview/Widget.preview.test.js
+++ client/src/Components/Dashboard/WidgetPreview/Widget.preview.test.js
@@ -1,7 +1,23 @@
 import { WidgetPreview } from "./Widget.preview";
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
-test('WidgetPreview renders widget-pv-ctn', () => {
+test('should render WidgetPreview', () => {
+    render(<WidgetPreview/>);
+})
+
+test('should render widget-pv-ctn', () => {
     const { container } = render(<WidgetPreview />);
     expect(container.firstChild.classList.contains('widget-pv-ctn')).toBe(true);
+})
+
+test('should render widget-or-calc', () => {
+    render(<WidgetPreview />)
+    const widgetOrCalc = screen.getByRole('widget-or-calc');
+    expect(widgetOrCalc).toBeEnabled(true);
+})
+
+test('should render widget-or-new', () => {
+    render(<WidgetPreview />)
+    const widgetOrNew = screen.getByRole('widget-or-new');
+    expect(widgetOrNew).toBeEnabled(true);
 })
