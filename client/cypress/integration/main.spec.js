1e919eb6fcb879c6cf0de1686bcb34177012af24
BartoszSzatkowski <szatek24@gmail.com>
2021-11-23 12:23:59 +0100

    test: add testing for toggling display mode and displayed result

--- client/cypress/integration/main.spec.js
+++ client/cypress/integration/main.spec.js
@@ -11,4 +11,25 @@ describe('E2E test', () => {
     cy.contains('Value field').parent().drag('.widget-dnd-ctn');
     cy.get('.input-ctn').should('be.visible');
   });
+
+  it('display inputed elements in setup mode', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains('New widget').click();
+    cy.contains('Value field').parent().drag('.widget-dnd-ctn');
+    cy.contains('Value field').parent().drag('.widget-dnd-ctn');
+    cy.contains('Value field').parent().drag('.widget-dnd-ctn');
+    cy.get('#toggle-mode').click();
+    cy.get('.element-setup-ctn').should('be.visible');
+  });
+
+  it('should display result properly', () => {
+    cy.visit('http://localhost:3000');
+    cy.contains('New widget').click();
+    cy.contains('Value field').parent().drag('.widget-dnd-ctn');
+    cy.get('#toggle-mode').click();
+    cy.get('#formula-input').type('A');
+    cy.get('#toggle-mode').click();
+    cy.get('.input-ctn').type(1);
+    cy.get('#result-display').should('have.contain', '1 USD');
+  });
 });
