67e65fe631d8275c023df325eea89615fe74b19b
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2021-11-24 12:09:57 +0100

    test: in progress add tests at app.test.js

---
+++ client/src/App.test.js
@@ -0,0 +1,15 @@
+import App from "./App"
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom";
+
+describe('App tests', () => {
+    test('should render App component', () => {
+        render(<BrowserRouter><App /></BrowserRouter>);
+        screen.debug();
+    })
+
+    test('should render App div', () => {
+        const { container } = render(<BrowserRouter><App /></BrowserRouter>);
+        expect(container.firstChild.classList.contains('App')).toBe(true);
+    })
+})
