19e074680292cd906e489b83a68e1b0ad780409c
BartoszSzatkowski <szatek24@gmail.com>
2021-11-24 10:32:02 +0100

    test: final tweaks in e2e testing

--- client/cypress/integration/main.spec.js
+++ client/cypress/integration/main.spec.js
@@ -1,20 +1,19 @@
 describe('E2E test', () => {
-  it('clicking widget tile takes user to widget builder', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:3000');
     cy.contains('New widget').click();
+  });
+
+  it('clicking widget tile takes user to widget builder', () => {
     cy.get('.widget-builder-ctn').should('be.visible');
   });
 
   it('lets to drag components to the widget builder field', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('New widget').click();
     cy.contains('Value field').parent().drag('.widget-dnd-ctn');
     cy.get('.input-ctn').should('be.visible');
   });
 
   it('display inputed elements in setup mode', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('New widget').click();
     cy.contains('Value field').parent().drag('.widget-dnd-ctn');
     cy.contains('Value field').parent().drag('.widget-dnd-ctn');
     cy.contains('Value field').parent().drag('.widget-dnd-ctn');
@@ -23,13 +22,11 @@ describe('E2E test', () => {
   });
 
   it('should display result properly', () => {
-    cy.visit('http://localhost:3000');
-    cy.contains('New widget').click();
     cy.contains('Value field').parent().drag('.widget-dnd-ctn');
     cy.get('#toggle-mode').click();
     cy.get('#formula-input').type('A');
     cy.get('#toggle-mode').click();
-    cy.get('.input-ctn').type(1);
+    cy.get('#widget-input').type(1);
     cy.get('#result-display').should('have.contain', '1 USD');
   });
 });
