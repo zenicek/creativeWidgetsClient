e70ffc6aa7518d5af70d2276a8ae4cd13849fe96
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2021-11-24 11:53:54 +0100

    test: added more tests to dashboard.test.js and widget.list.test.js

--- client/src/Components/Dashboard/Dashboard.test.js
+++ client/src/Components/Dashboard/Dashboard.test.js
@@ -11,3 +11,15 @@ test('should render dashboard-ctn', () => {
     const { container } = render(<BrowserRouter><Dashboard /></BrowserRouter>);
     expect(container.firstChild.classList.contains('dashboard-ctn')).toBe(true);
 })
+
+test('should render dash-side-menu', () => {
+    render(<BrowserRouter><Dashboard /></BrowserRouter>);
+    const dashSideMenu = screen.getByRole('dash-side-menu');
+    expect(dashSideMenu).toBeEnabled(true);
+})
+
+test('should render dash-widgets-ctn', () => {
+    render(<BrowserRouter><Dashboard /></BrowserRouter>);
+    const dashSideMenu = screen.getByRole('dash-widgets-ctn');
+    expect(dashSideMenu).toBeEnabled(true);
+})
--- client/src/Components/Dashboard/WidgetList/Widget.list.test.js
+++ client/src/Components/Dashboard/WidgetList/Widget.list.test.js
@@ -11,3 +11,9 @@ test('should render widget-list-ctn', () => {
     const { container } = render(<BrowserRouter><WidgetList /></BrowserRouter>);
     expect(container.firstChild.classList.contains('widget-list-ctn')).toBe(true);
 })
+
+test('should render widget-preview-link', () => {
+    render(<BrowserRouter><WidgetList /></BrowserRouter>);
+    const widgetPrevLink = screen.getByRole('widget-preview-link');
+    expect(widgetPrevLink).toBeEnabled(true);
+})
