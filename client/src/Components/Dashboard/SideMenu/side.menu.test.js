3189abc799cd564d272a8539e52a5568085d5568
OGALEXI <64463918+OGALEXI@users.noreply.github.com>
2021-11-24 11:58:11 +0100

    refactor: tests at side.menu.test.js

--- client/src/Components/Dashboard/SideMenu/side.menu.test.js
+++ client/src/Components/Dashboard/SideMenu/side.menu.test.js
@@ -3,38 +3,38 @@ import { SideMenu } from './side.menu';
 
 describe('SideMenu tests', () => {
     test('should render SideMenu', () => {
-    render(<SideMenu />);
-    screen.debug();
-})
+        render(<SideMenu />);
+        screen.debug();
+    })
 
-test('should render side-menu-wrapper-ctn', () => {
-    const { container } = render(<SideMenu />);
-    expect(container.firstChild.classList.contains('side-menu-wrapper-ctn')).toBe(true);
-})
+    test('should render side-menu-wrapper-ctn', () => {
+        const { container } = render(<SideMenu />);
+        expect(container.firstChild.classList.contains('side-menu-wrapper-ctn')).toBe(true);
+    })
 
-test('should render new-widget-btn', () => {
-    render(<SideMenu />);
-    const newWidgetBtn = screen.getByRole('new-widget-btn');
-    expect(newWidgetBtn).toBeEnabled(true);
-})
+    test('should render new-widget-btn', () => {
+        render(<SideMenu />);
+        const newWidgetBtn = screen.getByRole('new-widget-btn');
+        expect(newWidgetBtn).toBeEnabled(true);
+    })
 
-test('new-widget-btn has correct attribute', () => {
-    render(<SideMenu />);
-    expect(screen.getByText('+').closest('a')).toHaveAttribute('href', '/new-widget');
-})
+    test('new-widget-btn has correct attribute', () => {
+        render(<SideMenu />);
+        expect(screen.getByText('+').closest('a')).toHaveAttribute('href', '/new-widget');
+    })
 
-test('dash-link has correct attribute', () => {
-    render(<SideMenu />);
-    expect(screen.getByRole('dash-link').closest('a')).toHaveAttribute('href', '/');
-})
+    test('dash-link has correct attribute', () => {
+        render(<SideMenu />);
+        expect(screen.getByRole('dash-link').closest('a')).toHaveAttribute('href', '/');
+    })
 
-test('settings-link has correct attribute', () => {
-    render(<SideMenu />);
-    expect(screen.getByRole('settings-link').closest('a')).toHaveAttribute('href', '/');
-})
+    test('settings-link has correct attribute', () => {
+        render(<SideMenu />);
+        expect(screen.getByRole('settings-link').closest('a')).toHaveAttribute('href', '/');
+    })
 
-test('payment-link has correct attribute', () => {
-    render(<SideMenu />);
-    expect(screen.getByRole('payment-link').closest('a')).toHaveAttribute('href', '/');
-})
+    test('payment-link has correct attribute', () => {
+        render(<SideMenu />);
+        expect(screen.getByRole('payment-link').closest('a')).toHaveAttribute('href', '/');
+    })
 })
