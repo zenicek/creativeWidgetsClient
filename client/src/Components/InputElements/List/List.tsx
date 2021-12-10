0f272522fce4ef62039c099a2e5ce2213788a76c
Eugen Nikolajev <zenicek@gmail.com>
2021-12-10 12:08:12 +0100

    debug: list option

--- client/src/Components/InputElements/List/List.tsx
+++ client/src/Components/InputElements/List/List.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import { useIndividualWidgetContext } from '../../../Utils/Contexts';
 import './List.css';
-import Select from 'react-select';
+import Select, { SingleValue } from 'react-select';
 import { useArrangeElement } from '../../../Utils/CustomHooks';
 import InputProps from '../../../Types/InputProps';
 import Option from '../../../Types/Option';
@@ -11,7 +11,7 @@ export const List: React.FC<InputProps> = ({ id, index, moveElement }) => {
   const element = { ...findElement(id) };
   const [selectedOption, setSelectedOption] = useState(element.value);
 
-  const handleChange = (e: { value: number | string | null } | null) => {
+  const handleChange = (e: SingleValue<Option> | null) => {
     if (e?.value && element) {
       element.value = e.value;
       setSelectedOption(e.value);
